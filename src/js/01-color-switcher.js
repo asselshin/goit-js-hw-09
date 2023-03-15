b5d8b20bd20f7e2b9e1239352f15fc7a9378d7c9
Assel Shingissova <asselshin@gmail.com>
2023-03-15 14:22:31 +0600

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.body;
+let timerId = null;
 
+btnStart.addEventListener('click', () => {
+    timerId = setInterval(bodyColor, 1000);
+    btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+ });
+
+function bodyColor() {
+    body.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
